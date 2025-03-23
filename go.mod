6381af33c10315788543d8f07ac824bd56ec1657
CookieBorn <robanikin@hotmail.com>
2025-03-23 20:36:26 +1100

    A: Setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/CookieBorn/gator
+
+go 1.23.4
---
+++ internal/config/config.go
@@ -0,0 +1,9 @@
+package config
+
+func working(x string) string {
+	return x + "Works"
+}
+
+type strutConfig struct {
+	url string
+}
