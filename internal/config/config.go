93ec8e6b9767a45f81c6510c3b3bfd3024d49447
CookieBorn <robanikin@hotmail.com>
2025-03-24 22:31:44 +1100

    C: Config setup

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,12 @@
 package config
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
 func Working(x string) string {
 	return x + "Works"
 }
@@ -7,4 +14,45 @@ func Working(x string) string {
 type Config struct {
 	DB_URL   string `json:"db_url"`
 	Username string `json:"current_user_name"`
+}
+
+const configFileName = ".gatorconfig.json"
+
+func Read() (Config, error) {
+	var config Config
+	fileLocation, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Print("File Location Errot\n")
+		return config, err
+	}
+	fileLocation = fileLocation + configFileName
+	jsonFile, err := os.Open(configFileName)
+	if err != nil {
+		fmt.Print("File Open Error\n")
+		return config, err
+	}
+	defer jsonFile.Close()
+	data, err := io.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Print("File Read Error\n")
+		return config, err
+	}
+	json.Unmarshal(data, &config)
+	return config, nil
+}
+
+func (c *Config) SetUser(name string) {
+	c.Username = name
+	file, err := os.Open(configFileName)
+	if err != nil {
+		fmt.Print("Open File Error\n")
+	}
+	defer file.Close()
+	jsonData, err := json.Marshal(c)
+	if err != nil {
+		fmt.Print("Marshal Error\n")
+	}
+	fmt.Printf("%s\n", string(jsonData))
+	var fileMode os.FileMode
+	err = os.WriteFile(configFileName, jsonData, fileMode)
 }
--- main.go
+++ main.go
@@ -8,5 +8,6 @@ import (
 
 func main() {
 	fmt.Print("Have Fun\n")
-	fmt.Print(config.Working("Config ") + "\n")
+	file, _ := config.Read()
+	file.SetUser("robert")
 }
